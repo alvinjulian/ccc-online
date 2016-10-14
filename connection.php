a667ac76bec6352d25278ba90423bcdc8fb187f3
Alvin Julian <alvin.julian@outlook.com>
2016-10-14 16:04:49 +0700

    Connection
    Tambah file connection ke db server. Tinggal pakai

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+
+if($_POST['server_auth']=='cccOnlineAuth')
+{
+	$mysqli=mysqli_connect("http://ccconline.cloudapp.net","root","CCCOnline123","ccc_online") or die("Cannot Connect.");
+	
+	//Set Timezone
+	$query="set time_zone = '+7:00'";
+	$stmt=$mysqli->prepare($query);
+	$stmt->execute();
+	$stmt->close();
+}
+else
+{
+	echo "Could not reach server!";
+	die();
+}
+
+?>
